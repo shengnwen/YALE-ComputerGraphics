082ef6c91e59c3d5d9e8b9ad709835d995a8c644
shengnwen <shengwen@live.cn>
2015-09-10 08:34:45 -0400

    add some code in Q2

--- Assignment 1/q2/js/bilateral.js
+++ Assignment 1/q2/js/bilateral.js
@@ -1,60 +1,60 @@
-
-  // adapted from http://stackoverflow.com/questions/3914203/javascript-filter-image-color
-  function createCanvas(image){
+// adapted from http://stackoverflow.com/questions/3914203/javascript-filter-image-color
+function createCanvas(image) {
 
     // create a new canvas element
     var myCanvas = document.createElement("canvas");
     var myCanvasContext = myCanvas.getContext("2d");
 
 
-    var imgWidth=image.width;
-    var imgHeight=image.height;
+    var imgWidth = image.width;
+    var imgHeight = image.height;
+
 
     // set the width and height to show two copies of the image
-    myCanvas.width= 2*imgWidth+ 10;
+    myCanvas.width = 2 * imgWidth + 10;
     myCanvas.height = imgHeight;
 
     // draw the image
-    myCanvasContext.drawImage(image,0,0);
+    myCanvasContext.drawImage(image, 0, 0);
 
-    // get all the input and output image data into arrays 
-    var imageData = myCanvasContext.getImageData(0,0, imgWidth, imgHeight);
-	 var imoutData = myCanvasContext.getImageData(0,0, imgWidth, imgHeight);
+    // get all the input and output image data into arrays
+    var imageData = myCanvasContext.getImageData(0, 0, imgWidth, imgHeight);
+    var imoutData = myCanvasContext.getImageData(0, 0, imgWidth, imgHeight);
+
+    var filterSizeN = document.getElementById("filterSize");
+    var filterIntensityM = document.getElementById("filterIntensity");
 
 
     // go through it all...
-    for (j=0; j<imageData.width; j++)
-    {
-      for (i=0; i<imageData.height; i++)
-      {
-         // index: red, green, blue, alpha, red, green, blue, alpha..etc.
-         var index=(i*4)*imageData.width+(j*4);
-         var red=imageData.data[index];
-		 var green=imageData.data[index+1];
-		 var blue=imageData.data[index+2];
-         var alpha=imageData.data[index+3];
+    for (j = 0; j < imageData.width; j++) {
+        for (i = 0; i < imageData.height; i++) {
+            // index: red, green, blue, alpha, red, green, blue, alpha..etc.
+            var index = (i * 4) * imageData.width + (j * 4);
+            var red = imageData.data[index];
+            var green = imageData.data[index + 1];
+            var blue = imageData.data[index + 2];
+            var alpha = imageData.data[index + 3];
 
-         // set the red to the same
-         imoutData.data[index]=red;
+            // set the red to the same
+            imoutData.data[index] = red;
 
-         // set the rest to black
-         imoutData.data[index+1]=green*1.2;
-         imoutData.data[index+2]=blue*.8;
-         imoutData.data[index+3]=alpha;
-       }
-     }
+            // set the rest to black
+            imoutData.data[index + 1] = green * 1.2;
+            imoutData.data[index + 2] = blue * .8;
+            imoutData.data[index + 3] = alpha;
+        }
+    }
 
-     // put the image data back into the canvas
-     myCanvasContext.putImageData(imoutData,imageData.width+10,0,0,0, imageData.width,   imageData.height);
+    // put the image data back into the canvas
+    myCanvasContext.putImageData(imoutData, imageData.width + 10, 0, 0, 0, imageData.width, imageData.height);
 
     // append it to the body
-  document.body.appendChild(myCanvas);
-  }
-  function loadImage(){
+    document.body.appendChild(myCanvas);
+}
+function loadImage() {
     var img = new Image();
-    img.onload = function (){
-      createCanvas(img);
+    img.onload = function () {
+        createCanvas(img);
     }
     img.src = document.getElementById("imagefilename").value;
-  }
-  
+}
