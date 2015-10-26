8ea19748ec7a245252d937e246b7af0aee246f78
shengnwen <shengwen@live.cn>
2015-10-26 07:02:09 -0400

    q2

--- ShengWen-Assignment4/q3/js/primitives.js
+++ ShengWen-Assignment4/q3/js/primitives.js
@@ -3,3 +3,4 @@ document.write('<script type="text/javascript" src="js/tetrahedron.js"></script>
 document.write('<script type="text/javascript" src="js/sphere_latlong.js"></script>');
 document.write('<script type="text/javascript" src="js/sphere_subd.js"></script>');
 document.write('<script type="text/javascript" src="js/torus.js"></script>');
+document.write('<script type="text/javascript" src="js/b-spline.js"></script>');
