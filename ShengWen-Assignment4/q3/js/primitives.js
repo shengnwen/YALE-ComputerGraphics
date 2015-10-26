eabce3bc18eb131f010dc49a1211035724f14530
shengnwen <shengwen@live.cn>
2015-10-26 06:51:59 -0400

    q3 foundation

---
+++ ShengWen-Assignment4/q3/js/primitives.js
@@ -0,0 +1,5 @@
+document.write('<script type="text/javascript" src="js/cube-cone-cylinder.js"></script>');
+document.write('<script type="text/javascript" src="js/tetrahedron.js"></script>');
+document.write('<script type="text/javascript" src="js/sphere_latlong.js"></script>');
+document.write('<script type="text/javascript" src="js/sphere_subd.js"></script>');
+document.write('<script type="text/javascript" src="js/torus.js"></script>');
